8ebf9fc5d9b432306f1568fc25888263abb0c2cc
Namitha045 <namita.t4@gmail.com>
2022-06-08 14:55:00 -0400

    added tests and db configs

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+calculator function
+"""
+
+def add(x,y):
+    """Add x and y and return result"""
+    return x + y
+
+
+def subtract(x,y):
+    """subtract x from y and return result"""
+    return y - x
